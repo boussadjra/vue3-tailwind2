2d972705faad34e6f26b8d04dea629d49021457e
boussadjra <cb_boussadjra@esi.dz>
2021-02-14 00:50:47 +0100

    add inputs

--- src/components/form/WInput.tsx
+++ src/components/form/WInput.tsx
@@ -1,14 +1,91 @@
-import {  defineComponent} from 'vue';
+import { colorable, outlineable, shaped, sizeable, smoothable, variantable } from '@/mixins';
+import { defineComponent, PropType } from 'vue';
 
 export default defineComponent({
+	name: 'w-input',
 	props: {
-		
+		placeholder: {
+			type: String,
+			default: '',
+		},
+		label: {
+			type: String,
+			default: '',
+		},
+		variant: {
+			type: String as PropType<'default' | 'danger' | 'success' | 'warning'>,
+			default: 'default',
+		},
 	},
 
+	mixins: [colorable, outlineable, shaped, sizeable, smoothable, variantable],
 
-    render(){
-        return <input />
-    }
+	data() {
+		return {
+			uuid: '',
+			inputSizes: {
+				sm: 'h-8',
+				md: 'h-10',
+				lg: 'h-12',
+			},
+		};
+	},
+	created() {
+		this.uuid = `input${new Date().getTime()}`;
+	},
+	computed: {
+		classes() {
+			let classes: Array<string> = ['group flex items-center   focus-within:border-navy-blue-500 focus-within:text-navy-blue-500 '];
 
+			classes.push(this.inputSizes[this.size]);
+			classes.push(this.shapes[this.shape]);
 
+			if (this.smooth) {
+				classes = [
+					...classes,
+					this.bgSmoothColors[this.variant],
+					'text-gray-600 dark:text-gray-300',
+					this.bgSmoothColorsHover[this.variant],
+				];
+			} else if (this.outlined) {
+				classes = [
+					...classes,
+					'border bg-transparent',
+					this.borderColors[this.variant],
+					this.colors[this.variant],
+					this.borderColorsHover[this.variant],
+					this.borderColorsHover[this.variant],
+				];
+			}
+			return classes;
+		},
+		labelClasses() {
+			let classes = ['text-gray-600 dark:text-gray-400 font-light px-1'];
+			if (this.shape === 'rounded-full') {
+				classes.push('px-4');
+			}
+
+			return classes;
+		},
+	},
+
+	render() {
+		return (
+			<div class="space-y-2">
+				<label class={this.labelClasses} for={this.uuid}>
+					{this.label} 
+				</label>
+				<div class={this.classes}>
+					{this.$slots.prepend && <div class="text-md  pl-2 ">{this.$slots.prepend()}</div>}
+
+					<input
+						placeholder={this.placeholder}
+						id={this.uuid}
+						class="bg-transparent placeholder-gray-500 fo   px-4 h-full w-full outline-none focus:text-gray-600 focus:dark:text-gray-300 "
+					/>
+					{this.$slots.append && <div class="text-md mb-2 px-4 pt-4">{this.$slots.append()}</div>}
+				</div>
+			</div>
+		);
+	},
 });
--- src/views/components-demo/forms/Inputs.vue
+++ src/views/components-demo/forms/Inputs.vue
@@ -1,7 +1,127 @@
-<template>inputs</template>
+<template>
+  <w-grid gapY="16">
+    <div class="sub-title-1">Inputs :</div>
+    <w-grid :gapY="4">
+      <w-grid :gapY="3">
+        <div>Outlined inputs with different shapes :</div>
+        <w-grid :gap-x="4" :cols="3">
+          <w-input
+            label="Some label"
+            v-for="shape in shapes"
+            :key="shape"
+            :shape="shape"
+            placeholder="Input your text"
+            outlined
+          />
+        </w-grid>
+      </w-grid>
+    </w-grid>
+    <w-grid :gapY="6">
+      <w-grid :gapY="3">
+        <div>Smooth inputs with different shapes :</div>
+        <w-grid :gap-x="4" :cols="3">
+          <w-input
+            label="Some label"
+            v-for="shape in shapes"
+            :key="shape"
+            :shape="shape"
+            smooth
+            placeholder="Input your text"
+          />
+        </w-grid>
+      </w-grid>
+    </w-grid>
+    <w-grid :gapY="6">
+      <w-grid :gapY="3">
+        <div>Outlined inputs with different variants :</div>
+        <w-grid :gap="4" :cols="3">
+          <w-input
+            label="Some label"
+            v-for="variant in variants"
+            :key="variant"
+            :variant="variant"
+            outlined
+            placeholder="Input your text"
+          />
+        </w-grid>
+      </w-grid>
+    </w-grid>
+    <w-grid :gapY="6">
+      <w-grid :gapY="3">
+        <div>Smooth inputs with different variants :</div>
+        <w-grid :gap="4" :cols="3">
+          <w-input
+            label="Some label"
+            v-for="variant in variants"
+            :key="variant"
+            :variant="variant"
+            smooth
+            placeholder="Input your text"
+          />
+        </w-grid>
+      </w-grid>
+    </w-grid>
+    <w-grid :gapY="6">
+      <w-grid :gapY="3">
+        <div>Input with prepended icons:</div>
+        <w-grid :gap-x="4" :cols="3">
+          <w-input
+           
+            shape="rounded-full"
+            outlined
+            placeholder="Search"
+          >
+            <template #prepend>
+              <icon-search />
+            </template>
+          </w-input>
+          <w-input
+            label="Your phone number"
+     
+            outlined
+            placeholder="phone number"
+          >
+            <template #prepend>
+              <icon-phone />
+            </template>
+          </w-input>
+          <w-input
+            label="Your Address"
+     
+            outlined
+            placeholder="address"
+          >
+            <template #prepend>
+              <icon-location/>
+            </template>
+          </w-input>
+        </w-grid>
+      </w-grid>
+    </w-grid>
+  </w-grid>
+</template>
 
-<script>
-export default {};
+<script lang="ts">
+import WInput from "@/components/form/WInput";
+import IconLocation from "@/components/icons/IconLocation";
+import IconPhone from "@/components/icons/IconPhone";
+import IconSearch from "@/components/icons/IconSearch";
+import WGrid from "@/components/layout/Grid";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      shapes: ["rounded-none", "rounded", "rounded-full"],
+      variants: ["danger", "success", "warning"],
+    };
+  },
+  components: {
+    WGrid,
+    WInput,
+    IconSearch,IconPhone,IconLocation
+  },
+});
 </script>
 
 <style></style>
